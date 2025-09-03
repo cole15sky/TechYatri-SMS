2e2123e5e33584c5c5213b9ad865e69c44b0dc5b
Sky Blue <dawadiaakash@gmail.com>
2025-09-04 00:06:35 +0545

    Add .gitignore and remove __pycache__ files from Git

--- config/settings.py
+++ config/settings.py
@@ -77,10 +77,15 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'school_management',
+        'USER': 'root',      
+        'PASSWORD': 'techyatri123',  
+        'HOST': '127.0.0.1',          
+        'PORT': '3306',
     }
 }
+
 
 
 # Password validation
