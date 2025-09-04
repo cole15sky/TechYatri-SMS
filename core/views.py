99ea687f311079394bf4df8c12df96b90b5be725
Sky Blue <dawadiaakash@gmail.com>
2025-09-04 12:23:43 +0545

    feat(core/views): add SchoolViewSet for CRUD operations

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import School
+from .serializers import SchoolSerializer
 
-# Create your views here.
+class SchoolViewSet(viewsets.ModelViewSet):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
+
