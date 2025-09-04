4fc4e74081c02b71bdd87a472373d3e7b29b09f3
Sky Blue <dawadiaakash@gmail.com>
2025-09-04 12:27:57 +0545

    feat(core/serializers): add TeacherSerializer and StudentSerializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework import serializers
-from .models import School
+from .models import School, Teacher, Student
 
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
         fields = ['id', 'name', 'address']
+
+class TeacherSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Teacher
+        fields = ['id', 'name', 'subject'] 
+        
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ['id', 'name', 'age', 'grade', 'school']
