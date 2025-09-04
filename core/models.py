d7b145b9a69ccf01ee3d037940b0875109ddcbbc
Sky Blue <dawadiaakash@gmail.com>
2025-09-04 11:48:49 +0545

    feat(core): add teacher and student model

--- core/models.py
+++ core/models.py
@@ -6,3 +6,25 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+class Student(models.Model):
+    name = models.CharField(max_length=255)
+    age = models.IntegerField()
+    grade = models.CharField(max_length=50)
+    school = models.ForeignKey(
+        School,
+        on_delete=models.CASCADE,
+        related_name="students"
+    )
+    def __str__(self):
+        return f"{self.name} ({self.grade})"
+class Teacher(models.Model):
+    name = models.CharField(max_length=255)
+    subject = models.CharField(max_length=100)
+
+    school = models.ForeignKey(
+        School,
+        on_delete=models.CASCADE,
+        related_name="teachers"
+    )
+    def __str__(self):
+        return f"{self.name} - {self.subject}"
