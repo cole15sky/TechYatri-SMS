67911f4e3592ef1cec4c5145580130474542090a
Sky Blue <dawadiaakash@gmail.com>
2025-09-04 20:38:52 +0545

    chore(deps): add django-filter dependency

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'core',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
+    'django_filters',
 ]
 
 REST_FRAMEWORK = {
