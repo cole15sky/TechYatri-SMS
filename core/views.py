143efdda56df4a23257a782823c3707ae4fa131c
Sky Blue <dawadiaakash@gmail.com>
2025-09-04 20:45:32 +0545

    feat(student): add filtering by school and search by name/grade

--- core/views.py
+++ core/views.py
@@ -1,16 +1,19 @@
 from rest_framework import viewsets
 from .models import School, Teacher, Student
 from .serializers import SchoolSerializer, TeacherSerializer, StudentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+
 
 class SchoolViewSet(viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
-    
 class TeacherViewSet(viewsets.ModelViewSet):
     queryset = Teacher.objects.all()
     serializer_class = TeacherSerializer
-    
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
-
+    filter_backends = [DjangoFilterBackend, SearchFilter]
+    filterset_fields = ['school']
+    search_fields = ['name', 'grade']
