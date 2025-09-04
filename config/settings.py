1a15ea085ad6c4ed854598df058005fc412f47ec
Sky Blue <dawadiaakash@gmail.com>
2025-09-04 22:39:54 +0545

    feat(api): add pagination to Student API with PageNumberPagination

--- config/settings.py
+++ config/settings.py
@@ -55,6 +55,8 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'rest_framework.authentication.SessionAuthentication'
     ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
 }
 
 MIDDLEWARE = [
