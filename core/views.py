70f0af12d2b72dca94559d7e611ddc17cd708bc2
Sky Blue <dawadiaakash@gmail.com>
2025-09-04 12:32:33 +0545

    feat(core/views): add TeacherViewSet and StudentViewSet

--- core/views.py
+++ core/views.py
@@ -1,8 +1,16 @@
 from rest_framework import viewsets
-from .models import School
-from .serializers import SchoolSerializer
+from .models import School, Teacher, Student
+from .serializers import SchoolSerializer, TeacherSerializer, StudentSerializer
 
 class SchoolViewSet(viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
+    
+class TeacherViewSet(viewsets.ModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+    
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
 
